19525049ff8e995e6a2aa2c7f3ce90280190ccaf
Axel Martinez <sir_saeta@hotmail.com>
2021-11-11 01:14:06 -0300

    ajuste

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Text, View, useWindowDimensions, ScrollView, Button } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { styles, colors } from '../theme';
-import { Ionicons } from '@expo/vector-icons';
+import { styles } from '../theme';
 import { AddItem } from '../components/AddItem';
 import { Prestamo } from '../interfaces';
 import { ListItem } from '../components/ListItem';
@@ -11,7 +10,6 @@ import { ListItem } from '../components/ListItem';
 interface Props extends StackScreenProps<any, any>{};
 
 export const Home = ({navigation}: Props) => {
-    const { height, width } = useWindowDimensions()
     const [prestamos, setPrestamos] = useState<Prestamo[]>([])
     
     return (
@@ -24,8 +22,6 @@ export const Home = ({navigation}: Props) => {
                 </Text>
                 <AddItem prestamos={prestamos} setPrestamos={setPrestamos} />
                 <ListItem prestamos={prestamos} setPrestamos={setPrestamos} accionDetail={(params:Prestamo)=>navigation.navigate("DetailsLoans",params)} />
-                <Ionicons name="rocket" size={30} color="#900" />
-                <Button title='Ir Atras' onPress={() => navigation.canGoBack() && navigation.goBack()} />
             </View>
         </ScrollView>
     )
